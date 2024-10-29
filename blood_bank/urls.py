075d96e2c6d431ebdaa20cafe75cc83e2a48dbc8
Rubil Joshy <rubiljoshy@gmail.com>
2024-10-29 21:03:33 +0530

    Add initial project files

--- blood_bank/urls.py
+++ blood_bank/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+def home_view(request):
+    return HttpResponse("Welcome to the Blood Bank API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
